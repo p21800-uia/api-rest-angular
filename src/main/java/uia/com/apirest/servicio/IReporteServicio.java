6af56f73fde28c433f9135a109a97e0722ed6a34
p21800-uia <p21800@correo.uia.mx>
2022-03-10 20:22:35 -0600

    Primer commit

---
+++ src/main/java/uia/com/apirest/servicio/IReporteServicio.java
@@ -0,0 +1,15 @@
+package uia.com.apirest.servicio;
+
+import uia.com.apirest.modelo.ReporteModelo;
+import uia.com.apirest.modelo.ItemComprasUIAModelo;
+
+import java.util.ArrayList;
+
+public interface IReporteServicio {
+    ArrayList<ReporteModelo> getReportes();
+    Object getReporte(int id);
+    void print();
+
+    ReporteModelo deleteReportes(int id);
+    ReporteModelo putReporte(int id, ItemComprasUIAModelo newItem);
+}
---
+++ src/main/java/uia/com/apirest/servicio/RNSIServicio.java
@@ -0,0 +1,20 @@
+package uia.com.apirest.servicio;
+
+import uia.com.apirest.compras.GestorCompras;
+import uia.com.apirest.compras.InfoComprasUIA;
+
+import java.io.IOException;
+
+public class RNSIServicio implements IRNSIServicio
+{
+    private GestorCompras miGestorCompras = new GestorCompras();
+
+    public RNSIServicio() throws IOException {
+    }
+
+
+    @Override
+    public void agregaRNSI(InfoComprasUIA reporte) throws IOException {
+        this.miGestorCompras.agregaRNSI(reporte);
+    }
+}
